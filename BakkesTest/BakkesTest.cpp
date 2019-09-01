29389b5aaf5986a7e4b8a6f9945308b9d4158692
Gbo <gbo@siwhine.net>
2019-09-01 19:03:26 +0200

    Some stuff
    - picket a json library
    - got rid of boost
    - jsonification started
    - ws server is now a pointer and is constructed correctly no more crashes

--- BakkesTest/BakkesTest.cpp
+++ BakkesTest/BakkesTest.cpp
@@ -3,43 +3,36 @@
 #include "BakkesTest.h"
 #include "bakkesmod\plugin\bakkesmodplugin.h"
 using websocketpp::connection_hdl;
-using websocketpp::lib::placeholders::_1;
-using websocketpp::lib::placeholders::_2;
-using websocketpp::lib::bind;
+
 
 BAKKESMOD_PLUGIN(BakkesTest, "Stat n stuff over ws", "0.0.2", PLUGINTYPE_THREADED)
 
-using namespace std::placeholders;
-
 void BakkesTest::onLoad() {
+	// Hook events :
 	cvarManager->log("Some POC Plugin has been loaded :o why are you running this ?");
 	// Clean player stats from our OldPlayersState when game starts
-	gameWrapper->HookEventPost("Function TAGame.PlayerController_TA.PostBeginPlay", bind(&BakkesTest::ClearGoalStats, this, _1));
+	gameWrapper->HookEventPost("Function TAGame.PlayerController_TA.PostBeginPlay", std::bind(&BakkesTest::ClearGoalStats, this, std::placeholders::_1));
 	// If someones moves team ( or join, or you join and they're in a team ), updates stats
-	gameWrapper->HookEventPost("Function TAGame.GFxData_PRI_TA.HandleTeamChanged", bind(&BakkesTest::UpdateGoalStats, this, _1));
+	gameWrapper->HookEventPost("Function TAGame.GFxData_PRI_TA.HandleTeamChanged", std::bind(&BakkesTest::UpdateGoalStats, this, std::placeholders::_1));
 	// That's fired everytime someone scores. Also Pri.GetGoals is updated before this is fired
-	gameWrapper->HookEventPost("Function TAGame.ArenaSoundManager_TA.HandlePlayerScored", bind(&BakkesTest::UpdateGoalStats, this, _1));
-	
-	ws_server.init_asio();
-	ws_server.set_open_handler(bind(&BakkesTest::OnWsOpen, this, ::_1));
-	ws_server.set_close_handler(bind(&BakkesTest::OnWsClose, this, ::_1));
-	ws_server.listen(8323);
-	ws_server.start_accept();
-	ws_server.run();
-
+	gameWrapper->HookEventPost("Function TAGame.ArenaSoundManager_TA.HandlePlayerScored", std::bind(&BakkesTest::UpdateGoalStats, this, std::placeholders::_1));
+	RunWsServer();
 }
 
 void BakkesTest::OnWsOpen(connection_hdl hdl) {
+	// add a connection to the set
 	m_connections.insert(hdl);
 }
 
 void BakkesTest::OnWsClose(connection_hdl hdl) {
+	// remove a connection from the set
 	m_connections.erase(hdl);
 }
 
 void BakkesTest::SendWsPayload(std::string payload) {
+	// broadcast to all connections
 	for (auto it : m_connections) {
-		ws_server.send(it, payload, websocketpp::frame::opcode::text);
+		ws_server->send(it, payload, websocketpp::frame::opcode::text);
 	}
 }
 
@@ -50,14 +43,18 @@ void BakkesTest::ClearGoalStats(std::string eventName) {
 	}
 }
 void BakkesTest::UpdateGoalStats(std::string eventName) {
+	// Make sure we're online or NULL will strike :
 	if (gameWrapper->IsInOnlineGame()) {
-
+		// Get all Pris
 		ArrayWrapper<PriWrapper> players = gameWrapper->GetOnlineGame().GetPRIs();
 		for (unsigned int a = 0; a < players.Count(); a = a + 1) {
+			// If oldstate has a score < than current and event is scored -> fire goal_scored
 			if (!OldPlayersState[a].Dirty && eventName == "Function TAGame.ArenaSoundManager_TA.HandlePlayerScored") {
 				if (OldPlayersState[a].Goals < players.Get(a).GetMatchGoals()) {
 					gameWrapper->LogToChatbox(players.Get(a).GetPlayerName().ToString() + " has just scored!");
-					SendWsPayload(players.Get(a).GetPlayerName().ToString() + " has just scored!\n");
+					SendWsPayload(
+						GetGoalScoredEvent(players.Get(a))
+					);
 				}
 			}
 			OldPlayersState[a] = {
@@ -68,4 +65,44 @@ void BakkesTest::UpdateGoalStats(std::string eventName) {
 		}
 	}
 }
+string BakkesTest::GetGoalScoredEvent(PriWrapper player) {
+	json::JSON json_event;
+	json_event["event"] = "goal_scored";
+	json_event["player"] = JsonPriWrapper(player);
+	return json_event.dump();
+}
 
+json::JSON BakkesTest::JsonPriWrapper(PriWrapper player) {
+	json::JSON json_player;
+	json_player["PlayerName"] = player.GetPlayerName().ToString();
+	json_player["MatchGoals"] = player.GetMatchGoals();
+	json_player["MatchAssists"] = player.GetMatchAssists();
+	json_player["TeamInfo"] = BakkesTest::JsonTeamInfoWrapper(player.GetTeam());
+	return json_player;
+}
+
+json::JSON BakkesTest::JsonTeamInfoWrapper(TeamInfoWrapper team) {
+	json::JSON json_team;
+	json_team["TeamName"] = team.GetTeamName().ToString();
+	return json_team;
+}
+
+void BakkesTest::RunWsServer() {
+	// Init websocket server
+	if (ws_server == NULL) {
+		ws_server = new server();
+	}
+	else {
+		ws_server->stop();
+	}
+	ws_server->init_asio();
+	ws_server->set_open_handler(
+		websocketpp::lib::bind(&BakkesTest::OnWsOpen, this, websocketpp::lib::placeholders::_1)
+	);
+	ws_server->set_close_handler(
+		websocketpp::lib::bind(&BakkesTest::OnWsClose, this, websocketpp::lib::placeholders::_1)
+	);
+	ws_server->listen(8323);
+	ws_server->start_accept();
+	ws_server->run();
+}
--- BakkesTest/BakkesTest.h
+++ BakkesTest/BakkesTest.h
@@ -1,19 +1,25 @@
 #pragma once
 #pragma comment( lib, "BakkesMod.lib" )
 #include <set>
+#define ASIO_STANDALONE
+#define _WEBSOCKETPP_CPP11_TYPE_TRAITS_
 #include "websocketpp/config/asio_no_tls.hpp"
 #include "websocketpp/server.hpp"
 #include "bakkesmod\plugin\bakkesmodplugin.h"
-
+#include "json.hpp"
 using websocketpp::connection_hdl;
 
 typedef websocketpp::server<websocketpp::config::asio> server;
+typedef std::set<connection_hdl, std::owner_less<connection_hdl>> con_list;
+server* ws_server;
+con_list m_connections;
 
 
 struct PlayerState {
 	int Goals = 0;
 	string Name;
 	bool Dirty = true;
+
 };
 
 class BakkesTest : public BakkesMod::Plugin::BakkesModPlugin
@@ -25,10 +31,12 @@ public:
 	void ClearGoalStats(std::string eventName);
 	PlayerState OldPlayersState[16];
 private:
-	typedef std::set<connection_hdl, std::owner_less<connection_hdl>> con_list;
-	server ws_server;
-	con_list m_connections;
+	void RunWsServer();
 	void OnWsOpen(connection_hdl hdl);
 	void OnWsClose(connection_hdl hdl);
 	void SendWsPayload(string payload);
+	json::JSON JsonPriWrapper(PriWrapper player);
+	json::JSON JsonCarWrapper(CarWrapper car);
+	json::JSON JsonTeamInfoWrapper(TeamInfoWrapper team);
+	string GetGoalScoredEvent(PriWrapper player);
 };
